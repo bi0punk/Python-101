3e76517a5d87b2d7a5257bd930105a7f191b2253
sysbot <systecinfo_cl@criptext.com>
2022-01-02 23:05:38 -0300

    debbuging

--- for_registros.py
+++ for_registros.py
@@ -6,14 +6,12 @@ import bs4
 import requests
 import pandas as pd 
 
-
 #obteniendo sitio en htnl
 
 fuente_dat = 'http://www.sismologia.cl/ultimos_sismos.html'
 page = requests.get(fuente_dat)
 sopa = bs4.BeautifulSoup(page.text, 'lxml')
 """ print(sopa)  """
-
 
 tabla = sopa.find('table')
 
@@ -28,8 +26,6 @@ df = pd.DataFrame(columns = cabezeras )
 
 contador = 0
 
-
-
 for filas in tabla.find_all('td'):
     info = filas.find_all('td')
     if contador < 7:
@@ -42,13 +38,3 @@ for filas in tabla.find_all('td'):
 print(sismos)
 
 
-
-
-""" 
-for filas in tabla.find_all('tr')[1:]:
-    info = filas.find_all('td')
-    filas_info = [td.text.strip() for td in info]
-    length = len(df)
-    df.loc[length] = filas_info
-
-print(df) """
