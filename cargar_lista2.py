7b94a87d6bd4cc58281e4fe2c4b82acca9bed889
Henrry <74167958+bi0punk@users.noreply.github.com>
2023-05-10 22:29:14 -0400

    Update cargar_lista2.py

--- cargar_lista2.py
+++ cargar_lista2.py
@@ -1,12 +1,8 @@
-
-
-
-nalum=[]
-nombra=(raw_input("Ingresar Nombres de alumnos: (stop para finalizar):"))
-while nombra!="stop":
+nalum = []
+while True:
+    nombra = input("Ingresar Nombres de alumnos: (stop para finalizar): ")
+    if nombra == "stop":
+        break
     nalum.append(nombra)
-    nombra=(raw_input("Ingresar valor (stop para finalizar):"))
-    break
-print("Tamano de la lista:")
-print(len(nalum))
+print(f"Cantidad de alumnos registrados: {len(nalum)}")
 print(nalum)
