a89cbece94864f8d3235c1f822d2559d581a51ed
Henrry <74167958+bi0punk@users.noreply.github.com>
2023-05-10 22:34:40 -0400

    Update covid-tur.py

--- covid-tur.py
+++ covid-tur.py
@@ -1,14 +1,9 @@
-from turtle import * 
+from turtle import *
+
 color('green')
 bgcolor('black')
-speed(50)
-#hideturtle()
+speed('fastest')
 
-b=0
-while b < 200:
+for b in range(200):
     right(b)
     forward(b * 3)
-    b = b + 1
-    print(b)
-
-    
