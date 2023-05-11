2e366fad34b8c771c33ac607aff30687f1aa75fd
Henrry <74167958+bi0punk@users.noreply.github.com>
2023-05-10 22:44:06 -0400

    Update primos.py

--- primos.py
+++ primos.py
@@ -1,8 +1,19 @@
-print("numeros primos") 
-print("Ingrese un numero")
-num=int(input())
-a=num
+class Primo:
+    def __init__(self, num):
+        self.num = num
+        
+    def es_primo(self):
+        if self.num < 2:
+            return False
+        for i in range(2, int(self.num**0.5)+1):
+            if self.num % i == 0:
+                return False
+        return True
 
-if a/num ==0 and num%==0:
-	print("es primo")
-	
+print("Numeros primos")
+num = int(input("Ingrese un numero: "))
+primo = Primo(num)
+if primo.es_primo():
+    print("El numero es primo")
+else:
+    print("El numero no es primo")
