2ba7dbb43a7aab78bbd2a274e97a3a78654f060f
Henrry <74167958+bi0punk@users.noreply.github.com>
2023-05-10 22:42:14 -0400

    Update notas.py

--- notas.py
+++ notas.py
@@ -1,26 +1,21 @@
+print("Hola mundo calculadora")
+print("Cálculo de promedio")
+print("1.- Ingresar notas")
+print("2.- Calcular promedio")
 
-print("hola mundo calculadora")
-  
-print("Calculo de promedio")
-print(" 1.- Ingresar notas ")
-print(" 2.- calcular promedio")
+opcion = int(input())
 
-opcion=int(input())
-if opcion==1:
-
-	print("Ingrese nota 1 ")
-	pnum=int(input())
-	print("Ingrese el segundo numero")
-	snum=int(input())
-	resultado=pnum+snum
-	print("el resultado es ") +str(resultado)
-
-if opcion==2:
-	print("ha seleccionado la resta")
-	print("ingrese el primer numero")
-	rnum=int(input())
-	print("Ingrese el segundo numero")
-	r2num=int(input())
-	resultador=rnum-r2num
-	print("el resultado es ") +str(resultador)
-	
+if opcion == 1:
+    pnum = int(input("Ingrese nota 1: "))
+    snum = int(input("Ingrese nota 2: "))
+    resultado = pnum + snum
+    print(f"El resultado es: {resultado}")
+    
+elif opcion == 2:
+    rnum = int(input("Ingrese el primer numero: "))
+    r2num = int(input("Ingrese el segundo numero: "))
+    resultador = rnum - r2num
+    print(f"El resultado es: {resultador}")
+    
+else:
+    print("Opción no válida")
