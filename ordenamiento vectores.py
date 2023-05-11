f929852cbca052dc900fb7691b1659b0e0817ffe
Henrry <74167958+bi0punk@users.noreply.github.com>
2023-05-10 22:43:10 -0400

    Update ordenamiento vectores.py

--- ordenamiento vectores.py
+++ ordenamiento vectores.py
@@ -1,33 +1,17 @@
-# Este codigo ha sido generado por el modulo psexport 20160408-w32 de PSeInt.
-# Es posible que el codigo generado no sea completamente correcto. Si encuentra
-# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
-
 from random import randint
 
-if __name__ == '__main__':
-	arreglo = int()
-	i = int()
-	numin = int()
-	minimo = int()
-	j = int()
-	aux = int()
-	print("Ingrese la cantidad de datos que va a ingresar")
-	numin = int(input())
-	arreglo = [int() for ind0 in range(numin)]
-	print("Este es el vector original:")
-	print("")
-	for i in range(numin):
-		arreglo[i-1] = randint(0,9)
-		print(arreglo[i-1])
-	for i in range(numin):
-		minimo = i
-		for j in range(i,numin):
-			# con < ordena ascendente, con > ordena descendente
-			if arreglo[j-1]<arreglo[minimo-1]:
-				minimo = j
-		aux = arreglo[i-1]
-		arreglo[i-1] = arreglo[minimo-1]
-		arreglo[minimo-1] = aux
-	print("Este es el vector ordenado:")
-	for i in range(numin):          
-		print (arreglo[i-1])
+numin = int(input("Ingrese la cantidad de datos que va a ingresar: "))
+arreglo = [randint(0,9) for _ in range(numin)]
+
+print("Este es el vector original:")
+print(*arreglo, sep='\n')
+
+for i in range(numin):
+    minimo = i
+    for j in range(i,numin):
+        if arreglo[j] < arreglo[minimo]:
+            minimo = j
+    arreglo[i], arreglo[minimo] = arreglo[minimo], arreglo[i]
+
+print("Este es el vector ordenado:")
+print(*arreglo, sep='\n')
