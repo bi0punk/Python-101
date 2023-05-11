6660f3ddcb3541124d27507b1751ab9c4145a319
Henrry <74167958+bi0punk@users.noreply.github.com>
2023-05-10 22:54:33 -0400

    Update valida_rut.py

--- valida_rut.py
+++ valida_rut.py
@@ -1,23 +1,20 @@
 import sys
-from itertools import cycle
-def validarRut(rut):
-	rut = rut.upper();
-	rut = rut.replace("-","")
-	rut = rut.replace(".","")
-	aux = rut[:-1]
-	dv = rut[-1:]
-	revertido = map(int, reversed(str(aux)))
-	factors = cycle(range(2,8))
-	s = sum(d * f for d, f in zip(revertido,factors))
-	res = (-s)%11
-	if str(res) == dv:
-		return True
-	elif dv=="K" and res==10:
-		return True
-	else:
-		return False
-def main():
+import re
 
-	print(validarRut(sys.argv[1] ))
+def validarRut(rut):
+    rut = re.sub("[^0-9kK]", "", rut)
+    aux = rut[:-1]
+    dv = rut[-1]
+    factors = [2, 3, 4, 5, 6, 7]
+    factors.reverse()
+    digits = [int(d) for d in reversed(aux)]
+    s = sum(d * f for d, f in zip(digits, factors))
+    res = (11 - (s % 11)) % 11
+    valid_dv = str(res) if res < 10 else "K"
+    return valid_dv == dv
+
+def main():
+    print(validarRut(sys.argv[1]))
+
 if __name__=="__main__":
-	main()
+    main()
